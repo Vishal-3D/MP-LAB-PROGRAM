6a83d599457b375ede53ce43fec5f4d0e634f453
Vishal-3D <71490243+Vishal-3D@users.noreply.github.com>
2021-01-20 00:34:33 +0530

    Update stepperAnti.c

--- Microcontroller/Program1/stepperAnti.c
+++ Microcontroller/Program1/stepperAnti.c
@@ -3,7 +3,7 @@
 char xdata port _at_ 0xe803;
 char xdata porta _at_ 0xe800;
 charidataacc _at_ 0x30;
-delay() //DELAY BETWEEN THE ROTATION OF THE STEPPER MOTOR
+delay()
 {
 int j;
 for(j = 0;j<800; j++)
@@ -11,7 +11,7 @@ for(j = 0;j<800; j++)
 }
 void main()
 {
-port = 0x80; //CONFIGURE ALL THE PORTS OF 8255 AS OUTPUT PORT
+port = 0x80;
 while(1)
 {
 acc = 0x11;
